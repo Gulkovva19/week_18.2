32e6cfbbe35ca02bed7d722da60281b0c70e473b
Gulkovva19 <gulkova.sofya@yandex.ru>
2022-02-20 13:56:43 +0300

    homework

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-
 let errors = [];
+
 let checkValidity = (input) => {
+
+    event.preventDefault();
     let validity = input.validity;
 
     if (validity.valueMissing) {
-        errors.push('Поле' + input.placeholder + 'не заполнено');
+        errors.push('Поле ' + input.placeholder + ' не заполнено');
     }
     if (validity.rangeOverflow) {
-        let max = getAttributeValue(input, 'max');
-        errors.push(`Максимальное значение не может быть больше ${max}`);
+        let max = input.getAttribute('max');
+        errors.push(`Максимальное значение поля Number не может быть больше ${max}`);
     }
     if (validity.rangeUnderflow) {
-        let min = getAttributeValue(input, 'min');
-        errors.push(`Минимальное значение не может быть меньше ${min}`);
+        let min = input.getAttribute('min');
+        errors.push(`Минимальное значение поля Number не может быть меньше ${min}`);
     }
 }
 
@@ -23,6 +25,6 @@ let checkAll = () => {
     for (let input of inputs) {
         checkValidity(input);
     }
-
+    console.log(errors);
     document.getElementById('errorMessage').innerHTML = errors.join('.<br>');
 }
